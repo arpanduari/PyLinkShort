54841dcb6611e20422e7f3ef99162f072903d654
Arpan Duari <97849574+arpanduari@users.noreply.github.com>
2023-08-06 19:28:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from functools import partial
+
 import pyperclip
 import pyshorteners
 import ttkbootstrap as ttk
 import ttkbootstrap.dialogs.dialogs as msg
-from functools import partial
 
 
 class LinkShortener:
@@ -12,7 +13,7 @@ class LinkShortener:
         self.window = ttk.Window(themename='cyborg')
         self.window.resizable(False, False)
         self.window.title('PyLinkShort')
-        self.window.geometry('700x450')
+        self.window.geometry('700x500')
         # Widgets
         self.input_label = ttk.Label(
             master=self.window, text='Link Shortener', font='calibari 15 normal')
@@ -29,6 +30,8 @@ class LinkShortener:
         self.entry.pack(side='left', padx=10)
         self.shortener_btn.pack(side='left')
         self.input_url_frame.pack(pady=10)
+        self.short_links_frame = ttk.Frame(self.window)
+        self.short_links_frame.pack(pady=10)
         self.window.mainloop()
 
     def create_short_links(self, url):
@@ -42,22 +45,26 @@ class LinkShortener:
         }
         return shorted_links_dict
 
+    def clear_previous_links(self):
+        for widget in self.short_links_frame.winfo_children():
+            widget.destroy()
+
     def short_link(self):
         usr_url = self.entry_url.get()
         if len(usr_url) < 10:
-            msg.Messagebox.show_info(
-                'Hey this is not a valid url', 'Invalid URL')
+            msg.Messagebox.show_info('Hey this is not a valid url', 'Invalid URL')
         else:
 
             shorted_links_dict = self.create_short_links(usr_url)
+            self.clear_previous_links()
+            row_index = 0
             for service, value in shorted_links_dict.items():
-                temp_frame = ttk.Frame(master=self.window)
-                temp_frame.pack()
-                service_label = ttk.Label(
-                    master=temp_frame, text=f'{service} : {value}', font='Arial 12 normal')
+                temp_frame = ttk.Frame(master=self.short_links_frame)
+                temp_frame.grid(row=row_index, column=0, pady=10, sticky='w')
+                row_index += 1
+                service_label = ttk.Label(master=temp_frame, text=f'{service} :\t{value}', font='Arial 12 normal')
                 service_label.grid(row=0, column=0, pady=15, sticky='w')
-                copy_btn = ttk.Button(
-                    master=temp_frame, text='copy', command=partial(pyperclip.copy, value))
+                copy_btn = ttk.Button(master=temp_frame, text='copy', command=partial(pyperclip.copy, value))
                 copy_btn.grid(row=0, column=1, pady=15, padx=25, sticky='e')
 
 
